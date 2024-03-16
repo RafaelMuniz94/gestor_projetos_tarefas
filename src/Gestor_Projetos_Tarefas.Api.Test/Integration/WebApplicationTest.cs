8000d4bcdd5bf42aabc837cfde502d886409ef6b
Rafael Muniz <ramunmi94@gmail.com>
2024-03-16 13:37:11 -0300

    Created tests for UserServices and preparation for integration tests

---
+++ src/Gestor_Projetos_Tarefas.Api.Test/Integration/WebApplicationTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Gestor_Projetos_Tarefas.Database.Context;
+
+namespace Produtos_api.test.TestIntegracao
+{
+    // Criando um Factory customizado, dessa forma, sera possivel trocar o tipo de banco que utilizo para testes
+    // Evitando que o banco esteja sujo quando o teste de integracao executar.
+    // Essa classe ira utilizar reflecions para obter as configuracoes feitas na startup da api (Produtos_api/Program.cs)
+    public class WebApplicationTest<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+
+            builder.ConfigureServices(services =>
+            {
+
+                // As linhas abaixo irao limpar o contexto da startup e a conexao caso ela exista
+                var databaseContext = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<DataBaseContext>));
+                services.Remove(databaseContext);
+
+                var dbConnection = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbConnection));
+
+                services.Remove(dbConnection);
+
+                // Criando a conexao que sera utilizada apenas durante os testes de integracao
+                services.AddSingleton<DbConnection>(appContainer =>
+                {
+                    // Criando conexao em memoria
+                    var connection = new SqliteConnection("mode=memory;cache=shared");
+
+                    return connection;
+                });
+
+                // Inserindo conexao criada anteriormente no contexto
+                services.AddDbContext<DataBaseContext>((appContainer, options) => {
+                    var connection = appContainer.GetRequiredService<DbConnection>();
+                    options.UseSqlite(connection);
+                });
+
+
+            });
+            builder.UseEnvironment("Development");
+        }
+    }
+}
+
---
+++ src/Gestor_Projetos_Tarefas.Api.Test/Services/UserServicesTest.cs
@@ -0,0 +1,184 @@
+﻿using Xunit;
+using Moq;
+using Gestor_Projetos_Tarefas.Api.Services;
+using Gestor_Projetos_Tarefas.Api.Services.Interfaces;
+using Gestor_Projetos_Tarefas.Database.Interfaces;
+using Gestor_Projetos_Tarefas.Domain.Models;
+using Gestor_Projetos_Tarefas.Domain.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestor_Projetos_Tarefas.Test.Services
+{
+    public class UserServicesTests
+    {
+        [Fact]
+        public async Task ChangeTaskUser_ProjectNotFound_ReturnsFalse()
+        {
+            // Arrange
+            var projectID = Guid.NewGuid();
+            var oldUserID = Guid.NewGuid();
+            var newUserID = Guid.NewGuid();
+
+            var projectsRepositoryMock = new Mock<IProjectsRepository>();
+            projectsRepositoryMock.Setup(repo => repo.ReturnProject(It.IsAny<Guid>())).ReturnsAsync((Project)null);
+
+            var usersRepositoryMock = new Mock<IUsersRepository>();
+            var tasksRepositoryMock = new Mock<ITasksRepository>();
+
+            var userServices = new UserServices(projectsRepositoryMock.Object, usersRepositoryMock.Object, tasksRepositoryMock.Object);
+
+            // Act
+            var result = await userServices.ChangeTaskUser(oldUserID, newUserID, projectID);
+
+            // Assert
+            Assert.False(result);
+        }
+        [Fact]
+        public async Task ChangeTaskUser_RemoveProjectFromUser_ReturnsNull()
+        {
+            // Arrange
+            var projectID = Guid.NewGuid();
+            var oldUserID = Guid.NewGuid();
+            var newUserID = Guid.NewGuid();
+
+            var projectsRepositoryMock = new Mock<IProjectsRepository>();
+            projectsRepositoryMock.Setup(repo => repo.ReturnProject(It.IsAny<Guid>())).ReturnsAsync(new Project());
+
+            var usersRepositoryMock = new Mock<IUsersRepository>();
+            usersRepositoryMock.Setup(repo => repo.RemoveProjectFromUser(It.IsAny<Guid>(), It.IsAny<Guid>())).ReturnsAsync((bool?)null);
+
+           
+            var tasksRepositoryMock = new Mock<ITasksRepository>();
+
+            var userServices = new UserServices(projectsRepositoryMock.Object, usersRepositoryMock.Object, tasksRepositoryMock.Object);
+
+            // Act
+            var result = await userServices.ChangeTaskUser(oldUserID, newUserID, projectID);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task ChangeTaskUser_AddProject_ReturnsFalse()
+        {
+            // Arrange
+            var projectID = Guid.NewGuid();
+            var oldUserID = Guid.NewGuid();
+            var newUserID = Guid.NewGuid();
+
+           
+
+            var projectsRepositoryMock = new Mock<IProjectsRepository>();
+            projectsRepositoryMock.Setup(repo => repo.ReturnProject(It.IsAny<Guid>())).ReturnsAsync(new Project());
+
+            var usersRepositoryMock = new Mock<IUsersRepository>();
+            usersRepositoryMock.Setup(repo => repo.RemoveProjectFromUser(It.IsAny<Guid>(), It.IsAny<Guid>())).ReturnsAsync(true);
+            usersRepositoryMock.Setup(repo => repo.AddProject(It.IsAny<Guid>(), It.IsAny<Project>())).ReturnsAsync(false);
+
+
+            var tasksRepositoryMock = new Mock<ITasksRepository>();
+
+            var userServices = new UserServices(projectsRepositoryMock.Object, usersRepositoryMock.Object, tasksRepositoryMock.Object);
+
+            // Act
+            var result = await userServices.ChangeTaskUser(oldUserID, newUserID, projectID);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task ChangeTaskUser_AddProject_ReturnsTrue_RemoveProjectFromUser_ReturnsFalse()
+        {
+            // Arrange
+            var projectID = Guid.NewGuid();
+            var oldUserID = Guid.NewGuid();
+            var newUserID = Guid.NewGuid();
+
+
+
+            var projectsRepositoryMock = new Mock<IProjectsRepository>();
+            projectsRepositoryMock.Setup(repo => repo.ReturnProject(It.IsAny<Guid>())).ReturnsAsync(new Project());
+
+            var usersRepositoryMock = new Mock<IUsersRepository>();
+            usersRepositoryMock.Setup(repo => repo.RemoveProjectFromUser(It.IsAny<Guid>(), It.IsAny<Guid>())).ReturnsAsync(false);
+            usersRepositoryMock.Setup(repo => repo.AddProject(It.IsAny<Guid>(), It.IsAny<Project>())).ReturnsAsync(true);
+
+
+            var tasksRepositoryMock = new Mock<ITasksRepository>();
+
+            var userServices = new UserServices(projectsRepositoryMock.Object, usersRepositoryMock.Object, tasksRepositoryMock.Object);
+
+            // Act
+            var result = await userServices.ChangeTaskUser(oldUserID, newUserID, projectID);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task ChangeTaskUser_ReturnsTrue()
+        {
+            // Arrange
+            var projectID = Guid.NewGuid();
+            var oldUserID = Guid.NewGuid();
+            var newUserID = Guid.NewGuid();
+
+
+
+            var projectsRepositoryMock = new Mock<IProjectsRepository>();
+            projectsRepositoryMock.Setup(repo => repo.ReturnProject(It.IsAny<Guid>())).ReturnsAsync(new Project());
+
+            var usersRepositoryMock = new Mock<IUsersRepository>();
+            usersRepositoryMock.Setup(repo => repo.RemoveProjectFromUser(It.IsAny<Guid>(), It.IsAny<Guid>())).ReturnsAsync(true);
+            usersRepositoryMock.Setup(repo => repo.AddProject(It.IsAny<Guid>(), It.IsAny<Project>())).ReturnsAsync(true);
+
+
+            var tasksRepositoryMock = new Mock<ITasksRepository>();
+
+            var userServices = new UserServices(projectsRepositoryMock.Object, usersRepositoryMock.Object, tasksRepositoryMock.Object);
+
+            // Act
+            var result = await userServices.ChangeTaskUser(oldUserID, newUserID, projectID);
+
+            Assert.True(result);
+        }
+
+
+        [Fact]
+        public async Task ReturnUser_ValidUserID_ReturnsUserWithProjects()
+        {
+            // Arrange
+            var userID = Guid.NewGuid();
+            var user = new User { ID = userID };
+            var tasks = new List<ProjectTask>
+            {
+                new ProjectTask { ID = Guid.NewGuid(), User = userID, Project = Guid.NewGuid(), Status = ProjectTaskStatus.Concluida },
+                new ProjectTask { ID = Guid.NewGuid(), User = userID, Project = Guid.NewGuid(), Status = ProjectTaskStatus.EmAndamento },
+                new ProjectTask { ID = Guid.NewGuid(), User = userID, Project = Guid.NewGuid(), Status = ProjectTaskStatus.Pendente }
+            };
+            var projectIDs = tasks.Where(t => t.Status != ProjectTaskStatus.Concluida).Select(t => t.Project).ToList();
+
+            var projects = projectIDs.Select(projectID => new Project { ID = projectID }).ToList();
+
+            var projectsRepositoryMock = new Mock<IProjectsRepository>();
+            projectsRepositoryMock.Setup(repo => repo.ReturnProjectList(It.IsAny<List<Guid>>())).ReturnsAsync(projects);
+
+            var usersRepositoryMock = new Mock<IUsersRepository>();
+            usersRepositoryMock.Setup(repo => repo.ReturnUser(userID)).ReturnsAsync(user);
+
+            var tasksRepositoryMock = new Mock<ITasksRepository>();
+            tasksRepositoryMock.Setup(repo => repo.ReturnTasktListByUser(userID)).ReturnsAsync(tasks);
+
+            var userServices = new UserServices(projectsRepositoryMock.Object, usersRepositoryMock.Object, tasksRepositoryMock.Object);
+
+            // Act
+            var result = await userServices.ReturnUser(userID);
+
+            // Assert
+            Assert.Equal(user, result);
+            Assert.Equal(projects, result.Projects);
+        }
+    }
+}
